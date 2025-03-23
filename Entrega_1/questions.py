1ba96b185cdf5dc2a428b08ebb6d40887f00ebca
Nahir Ayelen Piancazza <nanu24lp@gmail.com>
2025-03-23 16:40:04 -0300

    Agrego sistema de puntaje

--- Entrega_1/questions.py
+++ Entrega_1/questions.py
@@ -22,6 +22,7 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+puntos = 0
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -41,11 +42,14 @@ for _ in range(3):
         user_answer = int(user_answer) - 1
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntos += 1
             break
         else:
             # Si el usuario no responde correctamente después de 2 intentos,
             # se muestra la respuesta correcta
             print("Incorrecto. La respuesta correcta es:")
             print(answers[question_index][correct_answers_index[question_index]])
+            puntos -= 0.5
         # Se imprime un blanco al final de la pregunta
         print()
+print(f"Puntuación final: {puntos}")
